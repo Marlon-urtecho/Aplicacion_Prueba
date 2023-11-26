d62b4bcc63e6a2677deac7e23397066f2003c489
Marlon-urtecho <urtechoalex065@gmail.com>
2023-11-25 18:49:44 -0600

    Inicio el proyecto creamos clases

---
+++ app/src/main/java/com/example/aplicacion_prueba/SinupActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aplicacion_prueba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SinupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sinup)
+    }
+}
